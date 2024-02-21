a9f8fa9f0929ee34e488a19fa18a049d850305d7
Dieselholic <gleb.kolosov@softeq.com>
2024-02-21 21:11:51 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Order> GetOrder()
+        {
+            return _context
+                .Orders
+                .OrderByDescending(x => x.Price * x.Quantity)
+                .AsNoTracking()
+                .FirstAsync();
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            return _context
+                .Orders
+                .Where(x => x.Quantity > 10)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<User> GetUser()
+        {
+            var topUser = _context
+                .Users
+                .Include(x => x.Orders)
+                .OrderByDescending(x => x.Orders.Count)
+                .AsNoTracking()
+                .First();
+
+
+            return _context
+                .Users
+                .OrderByDescending(x => x.Orders.Count)
+                .AsNoTracking()
+                .FirstAsync();
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            return _context
+                .Users
+                .Where(u => u.Status == UserStatus.Inactive)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
